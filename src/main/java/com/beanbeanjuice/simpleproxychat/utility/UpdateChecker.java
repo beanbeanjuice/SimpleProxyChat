f700e524a2730a2fade38719b8961e960dc12877
beanbeanjuice <beanbeanjuice@outlook.com>
2024-03-01 22:11:35 -0800

    Added Version Number to JAR

--- src/main/java/com/beanbeanjuice/simpleproxychat/utility/UpdateChecker.java
+++ src/main/java/com/beanbeanjuice/simpleproxychat/utility/UpdateChecker.java
@@ -2,16 +2,17 @@ package com.beanbeanjuice.simpleproxychat.utility;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class UpdateChecker {
 
     public static void checkUpdate(Consumer<String> functionToRun) {
-        try (InputStream is = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + 115305 + "/").openStream(); Scanner scann = new Scanner(is)) {
+        try (InputStream is = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + 115305 + "/").toURL().openStream(); Scanner scann = new Scanner(is)) {
             if (scann.hasNext()) functionToRun.accept(scann.next());
-        } catch (IOException ignored) { }
+        } catch (IOException | URISyntaxException ignored) { }
     }
 
 }
