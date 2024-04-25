7f91bae2eb16898f1640f6f14f70252c7ff425d4
beanbeanjuice <beanbeanjuice@outlook.com>
2024-04-25 01:58:34 -0700

    Updated Comment

--- src/main/java/com/beanbeanjuice/simpleproxychat/utility/listeners/bungee/BungeePreviousServerHandler.java
+++ src/main/java/com/beanbeanjuice/simpleproxychat/utility/listeners/bungee/BungeePreviousServerHandler.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 public class BungeePreviousServerHandler {
 
+    // TODO: Use UUID, String is insecure.
     private final HashMap<String, ServerInfo> previousServers = new HashMap<>();
 
     public void put(final String playerName, final ServerInfo serverInfo) {
