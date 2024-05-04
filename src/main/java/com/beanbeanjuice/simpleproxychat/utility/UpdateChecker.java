213dcc86066ce81e29d21d4f36c3417e4d80c35e
beanbeanjuice <beanbeanjuice@outlook.com>
2024-05-04 01:17:22 -0700

    Refactored Update Checker

--- src/main/java/com/beanbeanjuice/simpleproxychat/utility/UpdateChecker.java
+++ src/main/java/com/beanbeanjuice/simpleproxychat/utility/UpdateChecker.java
@@ -1,18 +1,49 @@
 package com.beanbeanjuice.simpleproxychat.utility;
 
+import com.beanbeanjuice.simpleproxychat.utility.config.Config;
+import com.beanbeanjuice.simpleproxychat.utility.config.ConfigDataKey;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class UpdateChecker {
 
-    public static void checkUpdate(Consumer<String> functionToRun) {
+    private final Config config;
+    private final String currentVersion;
+    private final Consumer<String> notifyFunction;
+
+    public UpdateChecker(final Config config, final String currentVersion, final Consumer<String> notifyFunction) {
+        this.config = config;
+        this.currentVersion = currentVersion;
+        this.notifyFunction = notifyFunction;
+    }
+
+    public Optional<String> getUpdate() {
         try (InputStream is = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + 115305 + "/").toURL().openStream(); Scanner scann = new Scanner(is)) {
-            if (scann.hasNext()) functionToRun.accept(scann.next());
+            if (scann.hasNext()) return Optional.of(scann.next());
         } catch (IOException | URISyntaxException ignored) { }
+        return Optional.empty();
+    }
+
+    public void checkUpdate() {
+        getUpdate().ifPresent((spigotVersion) -> {
+            if (currentVersion.equalsIgnoreCase(spigotVersion)) return;
+
+            String message = Helper.replaceKeys(
+                    config.getAsString(ConfigDataKey.UPDATE_MESSAGE),
+                    Tuple.of("plugin-prefix", config.getAsString(ConfigDataKey.PLUGIN_PREFIX)),
+                    Tuple.of("old", currentVersion),
+                    Tuple.of("new", spigotVersion),
+                    Tuple.of("link", "https://www.spigotmc.org/resources/115305/")
+            );
+
+            notifyFunction.accept(message);
+        });
     }
 
 }
