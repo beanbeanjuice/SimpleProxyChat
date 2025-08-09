5b85dd2df110990981e4abbdab8738ce29b208b0
William <64740551+beanbeanjuice@users.noreply.github.com>
2025-08-09 03:19:03 -0700

    feat: [GH-239] Add Release Please (#240)

--- projects/helper-app/build.gradle.kts
+++ projects/helper-app/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-version = "0.0.4"
+version = project.property("version") as String
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
